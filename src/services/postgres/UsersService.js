c7d9081d3ebcfa328371aeaa1c1d90684736d966
daisysere <daisyseredams@gmail.com>
2022-04-29 10:48:49 +0700

    update Userservice

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -2,6 +2,7 @@ const { nanoid } = require('nanoid');
 const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
 
 class UsersService {
   constructor() {
@@ -42,6 +43,21 @@ class UsersService {
       // InvariantError karena error ini termasuk kesalahan dalam bisnis logic yang tidak sesuai
       throw new InvariantError('Gagal menambahkan user. Username sudah digunakan');
     }
+  }
+
+  async getUserById(userId) {
+    const query = {
+      text: 'SELECT id, username, fullname FROM users WHERE id = $1',
+      values: [userId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('User tidak ditemukan');
+    }
+
+    return result.rows[0];
   }
 }
 
